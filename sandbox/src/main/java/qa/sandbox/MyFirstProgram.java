a344d228ae6b5046db8946fdb95e2efe54cd9caa
Denis Podoynikov <denisxer93@gmail.com>
2020-12-08 22:46:52 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/sandbox/MyFirstProgram.java
@@ -3,6 +3,28 @@ package qa.sandbox;
 class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("world");
+    hello("Zachpups");
+    hello("Dengaps");
+
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+    double a = 6;
+    double b = 7;
+
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double l) {
+    return l * l;
+  }
+  public static double area (double a, double b) {
+    return a * b;
   }
 }
+
