dc9405f17eae7ffae96cd70ace15e39d08be5488
Denisxer1613 <69155277+Denisxer1613@users.noreply.github.com>
2021-02-19 17:41:29 +0300

    Добавил примеры работы с массивами и списками

---
+++ dead_courses/src/main/java/ru/betboom/Primes.java
@@ -0,0 +1,23 @@
+package ru.betboom;
+
+public class Primes {
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+           if (n % i == 0) {
+               return false;
+           }
+        }
+        return true;
+    }
+
+
+    public static boolean isPrimeWhile(int n) {
+        int i = 2;
+        while (i < n && i != 0) {
+            i++;
+        }
+        return i == n;
+    }
+
+}
